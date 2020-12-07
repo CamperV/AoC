af8757608f12a9ba844c7ad307804206f267c5c4
Jay Danner <Jay.Danner@gtri.gatech.edu>
2020-12-07 11:59:57 -0500

    Updated day5, boardingpasses

---
+++ 2020/day5/day5_p2.kt
@@ -0,0 +1,45 @@
+// what's the highest boarding pass ID?
+import java.io.File
+
+fun main() {
+    val lines = File("input.txt").readLines()
+    
+    // max id of lines, skip first and last rows
+    var allPossiblePasses = (128..((128*8)-128)).toSet()
+    val allPasses = lines.map{ getBoardingPassID(it) }.toSet()
+
+    val outstanding = allPossiblePasses - allPasses
+    println("Found outstanding seat: $outstanding")
+}
+
+// honestly, since this is fixed-depth, let's not get fancy w/ recursion
+fun getBoardingPassID(bspString: String): Int {
+    val maxRows = 128
+    val maxCols = 8
+
+    var currRow = Pair(0, maxRows-1)
+    var currCol = Pair(0, maxCols-1)
+    for (designator in bspString) {
+        when(designator) {
+            'F' -> currRow = lowerHalf(currRow)
+            'B' -> currRow = upperHalf(currRow)
+            'L' -> currCol = lowerHalf(currCol)
+            'R' -> currCol = upperHalf(currCol)
+            else -> {
+                println("Invalid designator $designator in BSP-String $bspString")
+            }
+        }
+    }
+
+    return (currRow.first * 8) + currCol.first
+}
+
+fun lowerHalf(range: Pair<Int, Int>): Pair<Int, Int> {
+    val halfRange = (range.second - range.first + 1) / 2
+    return Pair(range.first, range.first + halfRange-1)
+}
+
+fun upperHalf(range: Pair<Int, Int>): Pair<Int, Int> {
+    val halfRange = (range.second - range.first + 1) / 2
+    return Pair(range.second - halfRange + 1, range.second)
+}
